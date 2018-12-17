05e09f3b4916da64db9f1ff3e1c6c34ea945715e
Anton Kikin <a.kikin@tano-systems.com>
2018-12-17 04:40:42 +0300

    Add link to application home in footer
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- luasrc/lldpd.lua
+++ luasrc/lldpd.lua
@@ -6,7 +6,12 @@
 module "luci.lldpd"
 
 local app_version = "1.2.0"
+local app_home = "https://github.com/tano-systems/luci-app-lldpd"
 
 function version()
 	return app_version
+end
+
+function home()
+	return app_home
 end
